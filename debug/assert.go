11c637fc76c1933289ac956c7701b311090bcb14
ghilbut <ghilbut@gmail.com>
2015-07-11 09:43:53 +0900

    refactoring assert.

--- debug/assert.go
+++ debug/assert.go
@@ -1,44 +1,50 @@
-package debug
+package assert
 
 import (
 	"fmt"
 	"log"
+	"reflect"
 	"runtime"
 )
 
 const kFormat = `
->>>>>>>> ASSERT >>>>>>>>
+>>>>>>>> [ygg.go] assert.%s >>>>>>>>
 [FILE] %s:%d\n
 [FUNC] %s
 %s
-<<<<<<<< ASSERT <<<<<<<<`
+<<<<<<<< [ygg.go] assert.%s <<<<<<<<`
 
-func getDetail() (file string, line int, function string) {
+func assert(t string, a ...interface{}) {
 	// NOTE(ghilbut):
 	// http://stackoverflow.com/questions/25927660/golang-get-current-scope-of-function-name
 
 	pc := make([]uintptr, 10) // at least 1 entry needed
 	runtime.Callers(3, pc)
 	f := runtime.FuncForPC(pc[0])
-	file, line = f.FileLine(pc[0])
-	function = f.Name()
-	return
+	file, line := f.FileLine(pc[0])
+
+	message := fmt.Sprint(a...)
+	log.Panicf(kFormat, t, file, line, f.Name(), message, t)
 }
 
-func Assert(condition bool, a ...interface{}) {
+func True(condition bool, a ...interface{}) {
 	if !condition {
-		file, line, function := getDetail()
-		msg := fmt.Sprint(a...)
-		log.Panicf(kFormat, file, line, function, msg)
+		assert("True", a)
 	}
 }
 
-/*
-func Assertf(condition bool, format string, a ...interface{}) {
-	if !condition {
-		file, line, function := getDetail()
-		msg := fmt.Sprintf(format, a...)
-		log.Panicf(kFormat, file, line, function, msg)
+func Contains(m interface{}, k interface{}, a ...interface{}) {
+
+	True(m != nil, "m should not be nil.", "\n", a)
+	True(k != nil, "k should not be nil.", "\n", a)
+
+	vm := reflect.ValueOf(m)
+	vk := reflect.ValueOf(k)
+
+	True(vm.Kind() == reflect.Map, "m should be map.", "\n", a)
+	True(vm.Type().Key() == vk.Type(), "k is not allowed key type.", "\n", a)
+
+	if vm.MapIndex(vk).Interface() == nil {
+		assert("Contains", a)
 	}
 }
-*/
--- debug/assert_test.go
+++ debug/assert_test.go
@@ -1,20 +1,12 @@
-package debug_test
+package assert_test
 
 import (
 	. "github.com/ghilbut/ygg.go/debug"
 	"testing"
 )
 
-func Test_assert_True_is_true(t *testing.T) {
-
-	defer func() {
-		if r := recover(); r != nil {
-			t.Fail()
-		}
-	}()
-
-	Assert(true, "this is assert test. this message is not allowed.")
-}
+const kDisplay = "[Test] this message should be displayed."
+const kNotDisplay = "[Test] this message should not be displayed."
 
 func Test_assert_True_is_false(t *testing.T) {
 
@@ -24,5 +16,53 @@ func Test_assert_True_is_false(t *testing.T) {
 		}
 	}()
 
-	Assert(false, "test message. this message should be displayed.")
+	True(false, kDisplay)
+}
+
+func Test_assert_True_is_true(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fail()
+		}
+	}()
+
+	True(true, kNotDisplay)
+}
+
+func Test_assert_Contains_when_check_invalid_type_key(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fail()
+		}
+	}()
+
+	target := make(map[string]bool)
+	Contains(target, 1, kDisplay)
+}
+
+func Test_assert_Contains_when_not_contained(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r == nil {
+			t.Fail()
+		}
+	}()
+
+	target := make(map[string]bool)
+	Contains(target, "key", kDisplay)
+}
+
+func Test_assert_Contains_when_contained(t *testing.T) {
+
+	defer func() {
+		if r := recover(); r != nil {
+			t.Fail()
+		}
+	}()
+
+	target := make(map[string]bool)
+	target["key"] = true
+	Contains(target, "key", kNotDisplay)
 }
